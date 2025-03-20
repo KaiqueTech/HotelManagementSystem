f417cbd1f1da0cb9864cbbcea41ca8661e6a41ed
KAIQUE SILVA <202404282091@alunos.unifavip.edu.br>
2025-03-20 20:52:01 -0300

    MUDANDO TELA DE LOGIN

--- HotelManagement/Telas/TelaLogin.Designer.cs
+++ HotelManagement/Telas/TelaLogin.Designer.cs
@@ -52,41 +52,46 @@
             // lblLogin
             // 
             lblLogin.AutoSize = true;
+            lblLogin.Font = new Font("Cascadia Mono SemiBold", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblLogin.Location = new Point(96, 211);
             lblLogin.Name = "lblLogin";
-            lblLogin.Size = new Size(40, 15);
+            lblLogin.Size = new Size(56, 17);
             lblLogin.TabIndex = 1;
             lblLogin.Text = "Login:";
             // 
             // txtLogin
             // 
             txtLogin.Location = new Point(96, 229);
+            txtLogin.Multiline = true;
             txtLogin.Name = "txtLogin";
-            txtLogin.Size = new Size(299, 23);
+            txtLogin.Size = new Size(299, 30);
             txtLogin.TabIndex = 2;
             // 
             // txtSenha
             // 
             txtSenha.Location = new Point(96, 295);
+            txtSenha.Multiline = true;
             txtSenha.Name = "txtSenha";
-            txtSenha.Size = new Size(299, 23);
+            txtSenha.Size = new Size(299, 30);
             txtSenha.TabIndex = 4;
             txtSenha.TextChanged += txtSenha_TextChanged;
             // 
             // lblSenha
             // 
             lblSenha.AutoSize = true;
+            lblSenha.Font = new Font("Cascadia Mono SemiBold", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblSenha.Location = new Point(96, 277);
             lblSenha.Name = "lblSenha";
-            lblSenha.Size = new Size(42, 15);
+            lblSenha.Size = new Size(56, 17);
             lblSenha.TabIndex = 3;
             lblSenha.Text = "Senha:";
             // 
             // btnEntrar
             // 
-            btnEntrar.Location = new Point(138, 373);
+            btnEntrar.Font = new Font("Cascadia Mono", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnEntrar.Location = new Point(96, 364);
             btnEntrar.Name = "btnEntrar";
-            btnEntrar.Size = new Size(75, 23);
+            btnEntrar.Size = new Size(117, 37);
             btnEntrar.TabIndex = 5;
             btnEntrar.Text = "Entrar";
             btnEntrar.UseVisualStyleBackColor = false;
@@ -94,9 +99,10 @@
             // btnSair
             // 
             btnSair.BackColor = SystemColors.Control;
-            btnSair.Location = new Point(266, 373);
+            btnSair.Font = new Font("Cascadia Mono", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnSair.Location = new Point(255, 364);
             btnSair.Name = "btnSair";
-            btnSair.Size = new Size(75, 23);
+            btnSair.Size = new Size(117, 37);
             btnSair.TabIndex = 6;
             btnSair.Text = "Sair";
             btnSair.UseVisualStyleBackColor = false;
